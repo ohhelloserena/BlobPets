4d6ee137722545acbce047a29b6d38fb625103f4
ohhelloserena <ohhelloserena@gmail.com>
2017-03-06 23:00:31 -0800

    added new scenes + connected profile to blobs

---
+++ ui/Assets/Scripts/EditCtrl.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement; 
+using System;
+using System.Net;
+
+/* Update User Profile*/
+
+
+public class EditCtrl : MonoBehaviour
+{
+
+	public int userId = -1;
+
+	public string url;
+
+	public string newName = "-1";
+	public string newPW = "-1";
+	public string confirmedPW = "-1";
+
+	public Text matched;
+
+	public void Start ()
+	{
+		printPasswordMsg();
+	}
+
+	/* 
+	If inputs are valid, calls updateUser() and then 
+	go to User Profile scene.
+	*/
+
+	public void Button_Click ()
+	{
+		Debug.Log ("Button clicked.");
+		//StartCoroutine(UpdateUser());
+		LoadScene ("UserProfileUI");
+
+	}
+
+	/*
+	Update user information by sending PUT request to API.
+	 */
+
+	 public void UpdateUser() {
+		
+	 }
+
+
+	
+
+	public void LoadScene (string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+
+
+	public void printPasswordMsg ()
+	{
+		if (checkPW ()) {
+			matched.text = "Passwords match.";
+		} else {
+			matched.text = "Passwords do not match.";
+		}
+	}
+
+
+	/*
+	Get new name for the user. 
+	
+	Input: 
+	nameField: new name inputted into name text field.
+	*/
+ 
+	public void getNewName (string nameField)
+	{
+		Debug.Log ("New name: " + nameField);
+		newName = nameField;
+	}
+
+	/*
+	Get new email for the user.
+
+	Input: 
+	- pwField: new password inputted into password field.
+	 */
+
+	public void getPW (string pwField)
+	{
+		Debug.Log ("PW: " + pwField);
+		newPW = pwField;
+	}
+
+
+	/*
+	Get confirmed email for the user.
+
+	Input: 
+	- pwField: confirmed email inputted into confirmed password field.
+	 */
+
+	public void getConfirmedPW (string pwField)
+	{
+		Debug.Log ("Confirmed PW: " + pwField);
+		confirmedPW = pwField;
+	}
+
+
+	/*
+	Return false if no new name entered or if new name only includes white spaces.
+	Else return true.
+	 */
+
+	public bool checkName ()
+	{
+		return false;
+		/*
+		string nameNoSpaces = newName.Replace (" ", string.empty);
+		Debug.Log ("nameNoSpaces: " + nameNoSpaces);
+		if (newName == "-1" || nameNoSpaces.Length < 1) {
+			return false;
+		} else {
+			return true;
+		}
+		*/
+	}
+
+	/*
+	Return false if no new password entered, if user doesn't fill out both password fields,
+	or if input for both fields don't match.
+	Else return true.
+	 */
+
+	public bool checkPW ()
+	{
+		if (newPW == "-1" || confirmedPW == "-1" || newPW != confirmedPW) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+
+
+}
---
+++ ui/Assets/Scripts/NewUser.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+// Create new user with given name, email, and password.
+
+public class NewUser : MonoBehaviour
+{
+
+	public string username = "";
+	public string email = "";
+	public string password = "";
+	public string userId = "";
+
+	public string url = "http://104.131.144.86/api/users/";
+
+	public string userToken = "";
+
+	// Use this for initialization
+	public void Start ()
+	{
+		
+	}
+
+	public void ButtonClick() {
+		CreateUser();
+	}
+
+
+
+/*
+	public void GetToken ()
+	{
+		string tokenUrl = "http://104.131.144.86/api/users/authenticate";
+		WWWForm form = new WWWForm ();
+		form.AddField ("email", "ryanchenkie@gmail.com");
+		form.AddField ("password", "secret");
+		WWW www = new WWW (tokenUrl, form);
+		StartCoroutine (WaitForRequest (www));
+	}
+
+	IEnumerator WaitForRequest (WWW www)
+	{
+		yield return www;
+
+		// check for errors
+		if (www.error == null) {
+			Debug.Log ("Token: " + www.text);
+			Debug.Log ("WWW Ok!");
+
+			
+		} else {
+			Debug.Log ("WWW Error: " + www.error);
+			if (www.error == "400 Bad Request") {
+				// alert for duplicate email address
+			}
+		}    
+	}
+	*/
+	
+
+	// POST request
+	public void CreateUser ()
+	{
+		WWWForm form = new WWWForm ();
+		form.AddField ("name", username);
+		form.AddField ("email", email);
+		form.AddField ("password", password);
+		WWW www = new WWW (url, form);
+		StartCoroutine (GetUserInfo (www));
+
+	}
+
+	IEnumerator GetUserInfo (WWW www)
+	{
+		yield return www;
+
+		// check for errors
+		if (www.error == null) {
+			userId = www.text;
+
+			Debug.Log ("WWW Ok! User created.");
+
+			Debug.Log ("*New userId: " + userId);
+			Debug.Log ("*Name: " + username);
+			Debug.Log ("*Email: " + email);
+			Debug.Log ("*Password: " + password);
+		} else {
+			Debug.Log ("WWW Error: " + www.error);
+			if (www.error == "400 Bad Request") {
+				// alert for duplicate email address
+			}
+		}    
+	}
+
+		
+	/*
+	Get input name for new user. 
+	
+	Input: 
+	nameField: new name inputted into name text field.
+	*/
+ 
+	public void getName (string inputField)
+	{
+		Debug.Log ("Name: " + inputField);
+		username = inputField;
+	}
+
+	/*
+	Get input email for new user.
+
+	Input: 
+	- pwField: new password inputted into password field.
+	 */
+
+	public void getEmail (string inputField)
+	{
+		Debug.Log ("Email: " + inputField);
+		email = inputField;
+	}
+
+	/*
+	Get input password for new user.
+
+	Input: 
+	- pwField: new password inputted into password field.
+	 */
+
+	public void getPassword (string inputField)
+	{
+		Debug.Log ("Password: " + inputField);
+		password = inputField;
+	}
+
+	
+}
