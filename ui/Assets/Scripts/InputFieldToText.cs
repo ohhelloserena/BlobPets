aa662f162c48b219125127e0715e8b7fca6f9b8d
janelle <janelle.sy@alumni.ubc.ca>
2017-03-08 14:06:48 -0800

    createblob screen adjustments, createblob script

---
+++ ui/Assets/Scripts/InputFieldToText.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class InputFieldToText : MonoBehaviour {
+
+	//	// Use this for initialization
+	//	void Start () {
+	//		
+	//	}
+	//	
+	//	// Update is called once per frame
+	//	void Update () {
+	//		
+	//	}
+	public string blobName = "";
+
+	public InputField Field;
+	public Text TextBox;
+
+	public string url = "http://104.131.144.86/api/users/";
+
+	//	public void setName() {
+	//		blobName = TextBox.text;
+	////		Debug.Log ("blobname: " + blobName); WORKS
+	//	}
+
+	// POST request
+	public void CreateBlob () {
+		blobName = TextBox.text;
+		Debug.Log ("blobname: " + blobName);
+		StartCoroutine (BlobPost ());
+	}
+
+	IEnumerator BlobPost () {
+		WWWForm form = new WWWForm();
+		form.AddField("name", blobName);
+		UnityWebRequest www = UnityWebRequest.Post(url, form);
+		yield return www.Send();
+
+		if(www.isError) {
+			Debug.Log(www.error);
+		}
+		else {
+			Debug.Log("Form upload complete!");
+		}
+	}
+}
