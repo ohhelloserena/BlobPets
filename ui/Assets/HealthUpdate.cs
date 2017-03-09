11b987cd8b5fbc22353a87d8d65c4ef185094cc9
janelle <janelle.sy@alumni.ubc.ca>
2017-03-08 19:17:44 -0800

    THE BAR WONT DECREASE HALP

---
+++ ui/Assets/HealthUpdate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUpdate : MonoBehaviour {
+	private Image healthBar;
+
+	public float health = 100.0f;
+	private const float coef = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		healthBar = GetComponent<Image>();
+		healthBar.fillAmount = 1;
+	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	void Update () {
+//		health -= coef * Time.deltaTime;
+		healthBar.fillAmount -= 0.0010f;
+		Debug.Log("Health : " + health);
+		Debug.Log("fill: " + healthBar.fillAmount);
+	}        
+}
