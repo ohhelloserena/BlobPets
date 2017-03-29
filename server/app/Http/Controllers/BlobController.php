667c3cc25476c19cad38d7ff9ed230501986310f
Robert Hsieh <robertcdcj@gmail.com>
2017-03-29 13:37:44 -0700

    Merged breed endpoint for blob into blob creation
    - will update test and remove endpoint in later commit

--- server/app/Http/Controllers/BlobController.php
+++ server/app/Http/Controllers/BlobController.php
@@ -176,9 +176,10 @@ class BlobController extends Controller
     public function createBlob(Request $request)
     {
         $maxNumBlobs = 4;
-        $expected = array('name', 'type', 'color');
+        $expectedCreateNew = array('name', 'type', 'color');
+        $expectedBreed = array('id1', 'id2');
         // check for correct number of inputs
-        if ($request->exists($expected)) {
+        if ($request->exists($expectedCreateNew)) {
             $blobName = $request->input('name');
             $blobType = $request->input('type');
             $blobColor = $request->input('color');
@@ -212,6 +213,42 @@ class BlobController extends Controller
             else {
                 return $ret;
             }
+        } else if ($request->exists($expectedBreed)) {
+
+            $parentBlob1 = $request->input('id1');
+            $parentBlob2 = $request->input('id2');
+
+            // check if owner is valid
+            $ret = $this->verifyUser();
+            if (is_int($ret)) {
+
+                if (empty(Blob::find($parentBlob1)) || empty(Blob::find($parentBlob2)) || Blob::find($parentBlob1)->owner_id != $ret || Blob::find($parentBlob2)->owner_id != $ret) {
+                    return response()->json(['error' => 'Invalid blob id'], 400);
+                }
+
+                $user = $ret;
+                $numBlobs = Blob::where('owner_id', $user)->count();
+
+                // check that user does not have the max number of blobs
+                if ($numBlobs < $maxNumBlobs){
+
+                    $blobName = 'Juvenile Blob';
+                    $blobType = 'A';
+                    $blobColor = 'red';
+                    $blob = Blob::create(array('name' => $blobName, 'type' => 'type ' .$blobType, 'owner_id' => $user, 'color' => $blobColor));
+                    $id = $blob->id;
+
+                    return response()->json(['blobID' => $id], 201);
+
+                }
+                else{
+                    return response()->json(['error' => 'User has max number of blobs'], 400);
+                }
+            }
+            else {
+                return $ret;
+            }
+
         } else {
             return response()->json(['error' => 'Did not have all required inputs'], 400);
         }
